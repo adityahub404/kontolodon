352bce7e3890d162f5e8c7bbb44508a5d4f36db5
adityahub404 <56800645+adityahub404@users.noreply.github.com>
2019-10-21 01:33:32 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$uid="Facebook_140408684013686";
+$raw_uid="d4be1613-0776-410e-a535-2f145e870f63";
+$user_name="Kocheng+Oren";
+$invite_code="FdryMb4j8";
+$user_token="AbyfsQpR76__2TDK";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="d7e3603395e147d5";
+$device_type="samsung++SM-G532G";
+
+
+?>
